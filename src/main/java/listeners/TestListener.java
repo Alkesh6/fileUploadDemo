399f86340109b180da6a8a8baa86b0e25f807ba2
Alkesh6 <tripathialkesh6@gmail.com>
2026-06-28 19:42:54 +0530

    Added listener and extent manager

--- src/main/java/listeners/TestListener.java
+++ src/main/java/listeners/TestListener.java
@@ -1,5 +1,58 @@
 package listeners;
 
-public class TestListener {
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
+import reports.ExtentManager;
+
+public class TestListener implements ITestListener {
+
+    private static ExtentReports extent = ExtentManager.getInstance();
+
+    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+
+    @Override
+    public void onStart(ITestContext context) {
+
+        System.out.println("========== Test Execution Started ==========");
+    }
+
+    @Override
+    public void onTestStart(ITestResult result) {
+
+        ExtentTest extentTest =
+                extent.createTest(result.getMethod().getMethodName());
+
+        test.set(extentTest);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+
+        test.get().pass("Test Passed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+
+        test.get().fail(result.getThrowable());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+
+        test.get().skip("Test Skipped");
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+
+        extent.flush();
+
+        System.out.println("========== Test Execution Finished ==========");
+    }
 }
--- src/main/java/reports/ExtentManager.java
+++ src/main/java/reports/ExtentManager.java
@@ -1,5 +1,50 @@
 package reports;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+import constants.frameworkConstants;
+
 public class ExtentManager {
+
+    private static ExtentReports extent;
+
+    private ExtentManager() {
+
+    }
+
+    public static ExtentReports getInstance() {
+
+        if (extent == null) {
+
+            String timeStamp =
+                    LocalDateTime.now()
+                            .format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
+
+            String reportPath =
+                    frameworkConstants.REPORT_PATH
+                            + "ExtentReport_"
+                            + timeStamp
+                            + ".html";
+
+            ExtentSparkReporter spark =
+                    new ExtentSparkReporter(reportPath);
+
+            spark.config().setDocumentTitle("Automation Report");
+
+            spark.config().setReportName("File Upload Automation");
+
+            extent = new ExtentReports();
+
+            extent.attachReporter(spark);
+
+        }
+
+        return extent;
+
+    }
 
 }
